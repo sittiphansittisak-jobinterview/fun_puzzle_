e0407d4320268a16be6898c7983d6a874aa3eff4
สิทธิพันธ์ สิทธิศักดิ์ <sittiphansittisak.jobinterview@gmail.com>
2023-07-24 20:10:04 +0700

    add multiplication table puzzle

---
+++ multiplication_table/main.dart
@@ -0,0 +1,24 @@
+final countStart = 1;
+final countStop = 12;
+
+void main() {
+  final input = 99; //You can change this!
+
+  final table = getTable(input);
+
+  showResult(input: input, table: table);
+}
+
+List<String> getTable(int number) {
+  final table = <String>[];
+  for (int count = countStart; count <= countStop; count++) {
+    table.add('$count x $number = ${count * number}');
+  }
+  return table;
+}
+
+void showResult({required input, required List<String> table}) {
+  print('input: $input');
+  print('output:');
+  table.forEach(print);
+}
